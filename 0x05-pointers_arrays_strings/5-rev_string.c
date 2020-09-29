20ceeecbce1ff59ca50accd0fb96c3fc75fe0393
Robert <rmagnus13@gmail.com>
2020-09-29 20:21:51 +0000

    fixed 1 and upload 5 & 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses string
+ * @s: str
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int front, back;
+	char *str = s;
+
+	for (front = 0; *(s + front); front++)
+	{
+		s(str + front) = *(s + front);
+	}
+
+	back = 0;
+	for (front = front - 1; front >= 0; front--)
+	{
+		*(s + front) = *(str + end);
+		end++;
+	}
+}
