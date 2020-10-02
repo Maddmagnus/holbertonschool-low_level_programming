c8534f9030f256a6c439ffca201bb17c1ea5ff9b
Robert <rmagnus13@gmail.com>
2020-10-02 02:23:38 +0000

    adding .h, read me, and 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concats strings
+ * @dest: destination
+ * @src: string
+ * Return: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
