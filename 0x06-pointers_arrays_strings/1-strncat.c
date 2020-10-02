c2dfdc24f12cf9fdeba0fa045df8bf7d39d0420a
Robert <rmagnus13@gmail.com>
2020-10-02 02:51:45 +0000

    adding 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strncat - concats two strings
+ * @dest: destination
+ * @src: source
+ * @n: copied part
+ */
+
+char *_strncat(char *dest,char *src, int n)
+{
+	int i = 0; j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+		dest[i]= '\0';
+	}
+	return (dest);
+}
