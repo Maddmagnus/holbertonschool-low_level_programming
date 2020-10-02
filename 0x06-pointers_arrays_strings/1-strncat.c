6169c1f3fd2901fe9dc8620608bc6cfc95d52ea5
Robert <rmagnus13@gmail.com>
2020-10-02 02:54:47 +0000

    fixed betty issues

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,14 +6,15 @@
  * @n: copied part
  */
 
-char *_strncat(char *dest,char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0; j;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;
 	for (j = 0; j < n && src[j] != '\0'; j++, i++)
 		dest[i] = src[j];
-		dest[i]= '\0';
+		dest[i] = '\0';
 	return (dest);
 }
