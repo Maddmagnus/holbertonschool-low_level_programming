a6812827bfa607691e2b699dc49a5dc82db87b8e
Robert <rmagnus13@gmail.com>
2020-10-02 04:21:40 +0000

    adding 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rot13 - converts string to rot13
+ * @s: string
+ * Return: converstion of string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char R13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = R13[j], j =52;
+			}
+		}
+	}
+	return(s);
+}
