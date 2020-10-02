5ef0f4fb8b44c6a452f34f5576fb150f6db5542d
Robert <rmagnus13@gmail.com>
2020-10-02 03:50:36 +0000

    fixed issues with betty

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,8 +9,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0';i++)
-		if (s[i] > 96 && s[i] < 123)
-			s[i] -= 32;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 96 && s[i] <= 123)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
 	return (s);
 }
