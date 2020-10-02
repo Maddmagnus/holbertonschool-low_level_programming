41e45eb39ff5bc0911f023c9b909603bcb55d8ee
Robert <rmagnus13@gmail.com>
2020-10-02 15:24:07 +0000

    fixed typo

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitilize words
+ * @str: string
+ * Return: capital words
+ */
+
+char *cap_string(char *s)
+{
+	int j = 0;
+        int i = 0;
+	char cap[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(',
+		      ')', '{', '}'};
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - ' ';
+	}
+	while (s[i] != '\0')
+	{
+		while (i < 13)
+		{
+			if (s[j] == cap[i])
+			{
+				if (s[j + 1] >= 'a' && s[j + 1] <= 'z')
+				{
+					s[j + 1]  = s[j + 1] - 32;
+				}
+			}
+				else
+				{
+					s[j] = s[j];
+				}
+			i++;
+		}
+		j++;
+	}
+	return (s);
+}
