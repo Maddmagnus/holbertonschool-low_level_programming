0fc104206d4b0c4cec0d633392b10dce8049f436
Robert <rmagnus13@gmail.com>
2020-10-06 04:02:22 +0000

    fixing 3 test 1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,12 +12,12 @@ unsigned int _strspn(char *s, char *accept)
 	int j = 0;
 	int counter = 0;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (count != i)
 			break;
 
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
