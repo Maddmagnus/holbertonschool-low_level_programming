cf26fbeb09a7127b4380c8c8810b53c907a61168
Robert <rmagnus13@gmail.com>
2020-10-06 04:41:28 +0000

    adding 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - set string value
+ * @s: pointer to change
+ * @to: string change
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
