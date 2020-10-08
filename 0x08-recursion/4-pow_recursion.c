bb1187a92cf0631a166b29a15bf31c43096f3415
Robert <rmagnus13@gmail.com>
2020-10-08 03:16:24 +0000

    added return value in comments

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,7 @@
  * _pow_recursion - value of x^y
  * @x: num
  * @y: exponent
+ * Return: x^y
  */
 
 int _pow_recursion(int x, int y)
@@ -13,7 +14,7 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 	if (y > 1)
-		return (x * _pow_recursion(x, y -1));
+		return (x * _pow_recursion(x, y - 1));
 	else
 		return (x);
 }
