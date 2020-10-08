b7a3806952d09dc2ce50583b73fcf1a33be48e73
Robert <rmagnus13@gmail.com>
2020-10-08 03:11:53 +0000

    adding 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - value of x^y
+ * @x: num
+ * @y: exponent
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	if (y > 1)
+		return (x * _pow_recursion(x, y -1));
+	else
+		return (x);
+}
