5c778da793aff493746ede76a27645283bcac70b
Robert <rmagnus13@gmail.com>
2020-10-08 03:09:54 +0000

    adding 3

---
+++ 0x08-recursion/3-fractorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns fractorial
+ * @n: number
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
