4fcfbf96dcd6778f8e53f130d0be6817619b6bd3
Robert <rmagnus13@gmail.com>
2020-10-12 04:52:39 +0000

    adding test file to work on later

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of coins
+ * @argc: number of args
+ * @argv: the array
+ * Return: number of coins needed
+ */
+
+int main(int argc, char *argv[])
+{
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (sum < 0)
+	{
+		printf("0\n")
+			}
+	printf("%d\n", coins);
+	return (0);
+}
