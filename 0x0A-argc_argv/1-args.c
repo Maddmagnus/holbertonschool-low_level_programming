a3c6718a53e4d0c2087628e36e273c3f7c37167c
Robert <rmagnus13@gmail.com>
2020-10-12 04:23:27 +0000

    adding 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: pointer to array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
