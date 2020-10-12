edaf83c56d5735d1f441e36eb71a79eaa37ad58d
Robert <rmagnus13@gmail.com>
2020-10-12 04:29:26 +0000

    adding 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints given arguments
+ * @argc: number of args
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *arv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
