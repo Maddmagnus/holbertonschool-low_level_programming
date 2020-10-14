37f9d66129932349b8369f6480e705250ba7d8fd
Robert <rmagnus13@gmail.com>
2020-10-14 18:55:35 +0000

    pld fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,30 +13,31 @@ int **alloc_grid(int width, int height)
 	int **array;
 	int i, j;
 
-	if (width < 1 || height < 1)
-		return(NULL);
-
-	array = malloc(sizeof(*array) * height);
-
-	if (array == NULL)
+	if (width <= 0 || height <= 0)
 	{
-		free(array);
 		return (NULL);
 	}
-
+	array = (int **)malloc(height * sizeof(int *));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(sizeof(*(array[i])) * width);
-		if (array[i] == 0)
+		array[i] = (int *)malloc(width * sizeof(int));
+		if (array[i] == NULL)
 		{
-			free(*array);
+			for (j = 0;j < i; j++)
+			{
+				free(array[j]);
+			}
 			free(array);
 			return (NULL);
 		}
-	}
-	for (j = 0; j < width; j++)
-	{
-		array[i][j] = 0;
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
 	}
 	return (array);
 }
