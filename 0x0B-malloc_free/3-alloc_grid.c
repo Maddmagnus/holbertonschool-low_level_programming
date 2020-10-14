a40a3f0d8bc947b90fbaa6effecc64e93499971c
Robert <rmagnus13@gmail.com>
2020-10-14 19:03:57 +0000

    betty on 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		array[i] = (int *)malloc(width * sizeof(int));
 		if (array[i] == NULL)
 		{
-			for (j = 0;j < i; j++)
+			for (j = 0; j < i; j++)
 			{
 				free(array[j]);
 			}
