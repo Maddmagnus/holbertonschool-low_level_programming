4ac08da7976251f41735a694c2dd0bf1de9cc67e
Robert <rmagnus13@gmail.com>
2020-10-16 04:24:40 +0000

    adding 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - allocates mem
+ * @nmemb: number
+ * @size: size
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+
+	untsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(nmemb * size);
+
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		m[i] = 0;
+	return (m);
+}
