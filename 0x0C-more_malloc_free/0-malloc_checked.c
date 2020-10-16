27486aea925cdb59d4dda86e4474a4dc5dbb8635
Robert <rmagnus13@gmail.com>
2020-10-16 04:05:36 +0000

    adding 0 & h

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - frees memory
+ * @b: object
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+		exit(98);
+
+	return(i);
+}
